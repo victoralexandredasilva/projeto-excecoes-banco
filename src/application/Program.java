8e639f6269fb4700dab0fb08e5ce13d4c76fdf2f
Victor Alexandre <victoralexandre2504@gmail.com>
2024-02-23 13:42:43 -0300

    Criada a pasta de excecoes, pagina de excecoes, e feito o tratamento de excecoes no programa, programa finalizado!

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,11 @@ package application;
 import java.util.Scanner;
 
 import model.entities.Account;
+import model.exceptions.DomainException;
 
 public class Program {
 	public static void main(String[] args) {
+		try {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Account data:");
 		System.out.print("Number:  ");
@@ -22,8 +24,17 @@ public class Program {
 		
 		System.out.print("Enter amount for withdraw:  ");
 		Double amount = sc.nextDouble();
+		acc.withdraw(amount);
 		
 		System.out.println(acc);
 		sc.close();
+		} 
+		catch(DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		catch(RuntimeException e) {
+			System.out.println("Ocorreu um erro inesperado!");
+		}
+
 	}
 }
--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 public class Account {
 	private Integer numberAccount;
 	private String holderAccount;
@@ -45,7 +47,13 @@ public class Account {
 		balance += amount;
 	}
 	
-	public void withdraw(Double amount) {
+	public void withdraw(Double amount) throws DomainException {
+		if(amount > withdrawLimit) {
+			throw new DomainException("The amount exceeds withdraw limit");
+		}
+		if(amount > balance) {
+			throw new DomainException("Not enough balance");
+		}
 		balance -= amount;
 	}
 	
